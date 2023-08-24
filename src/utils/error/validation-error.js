9808d5787f296e48acbcfba98b5541c8244e815f
VENOMJ-P <jay63837@gmail.com>
2023-08-24 10:21:22 +0530

    Added basic setup

---
+++ src/utils/error/validation-error.js
@@ -0,0 +1,15 @@
+class ValidationError extends Error {
+  constructor(error) {
+    super();
+    let explanation = [];
+    error.errors.forEach((error) => {
+      explanation.push(err.message);
+    });
+    this.name = "ValidationError";
+    this.message = "Not able to validate the data sent in the request";
+    this.explanation = explanation;
+    this.statusCode = StatusCodes.BAD_REQUEST;
+  }
+}
+
+module.exports = ValidationError;
